f422e7a64a850b8733642cf4fa8d41c63074c72f
baconwaifu <2634959+baconwaifu@users.noreply.github.com>
2020-07-26 00:51:47 -0400

    Move frontend to `vw.py` and test module enumeration (it crashes the simulator...)

--- vw.py
+++ vw.py
@@ -1,6 +1,67 @@
 import kwp
 import can
-import vag-block
+import vwtp
+import queue
+
+class blockMeasure:
+  def __init__(self, name, func):
+    self.func = func
+    self.name = name
+    self.label = None
+  def unscale(self, a, b):
+    ret = blockMeasure(self.name, None)
+    ret.value = self.func(a,b)
+    return ret
+  def __str__(self):
+    if self.label:
+      return "{} {} {}".format(self.value, self.name, self.label)
+    else:
+      return "{} {}".format(self.value, self.name)
+  def __repr__(self):
+    return self.__str__()
+
+scalers = {
+0x1: blockMeasure("/min", lambda a,b: (a*b)/5),
+0x4: blockMeasure("°ATDC", lambda a,b: (b-127)*.01*a), #BTDC is expressed as a negative number.
+0x7: blockMeasure("km/h", lambda a,b: .01*a*b),
+0x8: blockMeasure("binary", lambda a,b: (a << 8) | b),
+0x10: blockMeasure("binary", lambda a,b: (a << 8) | b),
+0x11: blockMeasure("ASCII", lambda a,b: bytes([a,b]).decode("ascii")),
+0x12: blockMeasure("mbar", lambda a,b: (a*b)*25),
+0x14: blockMeasure("%", lambda a,b: ((a*b)/128)-1),
+0x15: blockMeasure("V", lambda a,b: (a*b)/1000),
+0x16: blockMeasure("ms", lambda a,b: .001*a*b),
+0x17: blockMeasure("%", lambda a,b: (b*a)/256),
+0x19: blockMeasure("g/s (air)", lambda a,b: (100/a)*b),
+0x1A: blockMeasure("°C", lambda a,b: b-a),
+0x21: blockMeasure("%", lambda a,b: b*100 if a == 0 else (b*100)/a), #same unit, different scaling.
+0x22: blockMeasure("kW", lambda a,b: (b - 128)*.01*a),
+0x23: blockMeasure("/h", lambda a,b: (a*b)/100),
+0x25: blockMeasure("binary", lambda a,b: (a << 8) | b),
+0x27: blockMeasure("mg/stroke (fuel)", lambda a,b: (a*b)/256),
+0x31: blockMeasure("mg/stroke (air)", lambda a,b: (a*b)/40),
+0x33: blockMeasure("mg/stroke (Δ)", lambda a,b: ((b-128)/255)*a),
+0x36: blockMeasure("Count", lambda a,b: (a*256)+b),
+0x37: blockMeasure("seconds", lambda a,b: (a*b)/200),
+0x51: blockMeasure("°CF", lambda a,b: ((a*11200)+(b*436))/1000), #torsion; check formula.
+0x5E: blockMeasure("Nm", lambda a,b: a*((b/50)-1)), #torque; check formula.
+0x100: blockMeasure("[Unknown Unit]", lambda a,b: (a << 8) | b)
+}
+
+
+def parseBlock(block):
+  blk = []
+  buf = block[2:]
+  for i in range(0, len(buf), 3):
+    if buf[i] in scalers:
+      blk.append(scalers[buf[i]].unscale(buf[i+1], buf[i+2]))
+    else:
+      blk.append(scalers[256].unscale(buf[i+1], buf[i+2]))
+  return blk
+
+def labelBlock(ecu, blknum, blk):
+  for i in range(len(blk)):
+    blk.label = labels[(ecu,blknum)][i]
 
 modules = {
 0x1: "Engine",
@@ -8,7 +69,7 @@ modules = {
 0x3: "ABS",
 0x5: "Security Access",
 0x6: "Passenger Seat",
-0x7: "Front Infotainment"
+0x7: "Front Infotainment",
 0x8: "Climate Control",
 0x9: "Central Electronics",
 0x10: "Parking Aid #2",
@@ -60,9 +121,10 @@ class VWVehicle:
 
   def enum(self): #a crude enumeration primitive of all *known* ECUs
     global modules
-    for mod in modules.keys:
+    for mod in modules.keys():
       try:
         stack.connect(mod).close()
+        print("Found module:",modules[mod])
         self.enabled.append(mod)
       except queue.Empty:
         pass #squash the exception; just means "module not detected"
@@ -71,10 +133,17 @@ if __name__ == "__main__":
   sock = can.interface.Bus(channel='vcan0', bustype='socketcan')
   stack = vwtp.VWTPStack(sock)
 
-  conn = stack.connect(modules["ECU"])
+  car = VWVehicle(stack)
+  print("Connecting to vehicle and enumerating modules, please wait a moment.")
+  car.enum()
+  print("Modules present:")
+  for k in car.enabled:
+    print(" ",modules[k])
+
+  conn = stack.connect(0x01)
   kw = kwp.KWPSession(conn)
   with conn:
     assert kw.request("startDiagnosticSession", 0x89) == b'\x50\x89' #positive response, same value.
-    print(vag-block.parseBlock(kw.request("readDataByLocalIdentifier", 0x14)))
+    print(parseBlock(kw.request("readDataByLocalIdentifier", 2)))
   kw.close()
   import sys; sys.exit(0) #need to do this because of threads.
