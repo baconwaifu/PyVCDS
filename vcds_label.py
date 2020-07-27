7b23437810953c14344df481097e2f4f7f6e20fb
baconwaifu <2634959+baconwaifu@users.noreply.github.com>
2020-07-26 21:50:54 -0400

    add stub VCDS label parser

---
+++ vcds_label.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+#a VCDS .lbl parser. returns a dict module dicts, with those containing "dtc", "block" labels.
+#the latter being an array of n+1 elements, where 'n' is the number of measurements in the block.
+#the first element is the name of the block.
+
+#TODO
